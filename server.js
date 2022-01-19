df7825a5c162d4d329eae62b294bb33c07c7145a
Jon Chastain <chastain.r.jon@gmail.com>
2022-01-19 17:47:52 -0500

    added connection to db

--- server.js
+++ server.js
@@ -8,10 +8,47 @@ const app = express()
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
-app.use((req, res) => {
-    res.status(404).end()
-})
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows)
+// })
+
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(row)
+// })  
+
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(result)
+// })
+
+// app.use((req, res) => {
+//     res.status(404).end()
+// })
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`
+// const params = [1, 'Ronald', 'Firbank', 1]
+// db.query(sql, params, (err,result)=>{
+//     if(err){
+//         console.log(err)
+//     }
+//     console.log(result)
+// })
 
 //starts server on port 3001
 app.listen(PORT, () => {
